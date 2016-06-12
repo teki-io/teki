3de2b77d69c98610e2b84a95620cd10a0e43ed91
Neil Ni <tzuyangni@gmail.com>
2016-06-11 23:26:41 -0400

    feature: set up shift settings drag and drop in frontend

--- client/src/client/app/+shift-settings/shift-settings.ts
+++ client/src/client/app/+shift-settings/shift-settings.ts
@@ -8,12 +8,16 @@ import { Row }                from './components/row/index';
 import { Headers }            from './components/headers/index';
 import { NewRow }             from './components/new-row/index';
 import * as Service           from '../shared/services/index';
+import { Dragula, DragulaService } from 'ng2-dragula/ng2-dragula';
+import { TimerWrapper } from 'angular2/src/facade/async';
+
 
 @BaseComponent({
   selector: 'teki-shift-settings',
   templateUrl: 'app/+shift-settings/shift-settings.html',
   styleUrls: ['app/+shift-settings/shift-settings.css'],
-  directives: [Widget, WidgetBody, WidgetHeader, Row, Headers, NewRow, AppLayoutComponent],
+  directives: [Widget, WidgetBody, WidgetHeader, Row, Headers, NewRow, AppLayoutComponent, Dragula],
+  viewProviders: [DragulaService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
@@ -21,7 +25,12 @@ import * as Service           from '../shared/services/index';
 export class ShiftSettingsComponent {
   adding: boolean = false;
 
-  constructor(private shiftTemplateService: Service.Admin.ShiftTemplate) {}
+  constructor(
+    private shiftTemplateService: Service.Admin.ShiftTemplate,
+    private dragulaService: DragulaService) {
+    dragulaService.drag.subscribe((value: any) => this.onDrag(value.slice(1)));
+    dragulaService.drop.subscribe((value: any) => this.onDrop(value.slice(1)));
+  }
 
   ngOnInit() {
     this.shiftTemplateService.load();
@@ -37,5 +46,34 @@ export class ShiftSettingsComponent {
 
   editCancel() {
     this.cancel();
+  }
+
+  private hasClass(el: any, name: string): boolean {
+    return el.className.indexOf(name) > 0;
+  }
+
+  private addClass(el: any, name: string) {
+    if (!this.hasClass(el, name)) {
+      el.className = el.className ? [el.className, name].join(' ') : name;
+    }
+  }
+
+  private removeClass(el: any, name: string) {
+    if (this.hasClass(el, name)) {
+      el.className = el.className.replace(name, '');
+    }
+  }
+
+  private onDrag(args) {
+    let [e] = args;
+    this.removeClass(e, 'ex-moved');
+  }
+
+  private onDrop(args) {
+    let [e] = args;
+    this.addClass(e, 'ex-moved');
+    TimerWrapper.setTimeout(() => {
+      this.removeClass(e, 'ex-moved');
+    }, 200);
   }
 }
